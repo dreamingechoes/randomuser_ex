9d624f08a28af45e775c5d3aad0ad83c61c951a9
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-16 15:54:11 +0200

    Add some dependencies and configurations in mix.exs

--- mix.exs
+++ mix.exs
@@ -6,23 +6,52 @@ defmodule RandomuserEx.Mixfile do
       app: :randomuser_ex,
       version: "0.1.0",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      package: package(),
+      description: description(),
+      deps: deps(),
+      docs: [
+        main: "RandomuserEx"
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      applications: [:logger, :httpoison, :exjsx]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:exjsx, "~> 4.0", app: false, override: true},
+      {:httpoison, "~> 0.6"},
+      {:excoveralls, "~> 0.7", only: :test},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
+  end
+
+  def package do
+    [
+      maintainers: ["dreamingechoes"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/dreamingechoes/randomuser_ex"}
+    ]
+  end
+
+  def description do
+    "Elixir wrapper to consume randomuser.me API"
   end
 end
