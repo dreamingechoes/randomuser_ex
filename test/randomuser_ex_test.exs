517a4fbbac3fb2009d4ccc4a1a19128e8bdde11b
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-16 19:52:25 +0200

    Add some minimal tests

--- test/randomuser_ex_test.exs
+++ test/randomuser_ex_test.exs
@@ -1,8 +1,18 @@
 defmodule RandomuserExTest do
   use ExUnit.Case
+
   doctest RandomuserEx
 
-  test "greets the world" do
-    assert RandomuserEx.hello() == :world
+  test "get_one/1 with some params returns one random user" do
+    {:ok, user} = RandomuserEx.get_one(%{gender: :female})
+
+    assert !is_nil(user)
+  end
+
+  test "get_many/2 with some params returns 10 random users" do
+    {:ok, users} = RandomuserEx.get_many(%{gender: :male}, 10)
+
+    assert !is_nil(users)
+    assert length(users) == 10
   end
 end
