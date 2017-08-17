d5dd188993168212728aa600921d9ca0b460a51b
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-17 14:59:10 +0200

    Update version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RandomuserEx.Mixfile do
   def project do
     [
       app: :randomuser_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
